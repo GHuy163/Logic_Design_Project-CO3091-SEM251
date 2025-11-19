24afeaa9d9cc60711d00108db38a2fe9d8d024aa
TanLee24 <tandepzai24@gmail.com>
2025-11-19 21:18:50 +0700

    update

--- YoloUNO_PlatformIO-RTOS_Project/src/main.cpp
+++ YoloUNO_PlatformIO-RTOS_Project/src/main.cpp
@@ -22,7 +22,8 @@ void setup()
 {
   Serial.begin(115200);
   check_info_File(0);
-
+  
+  /*************************************** Tan Le's tasks *************************************/
   xTaskCreate(Wifi_Task, "Task WIFI", 8192, NULL, 3, NULL);
   xTaskCreate(wifi_monitor_task, "WiFiMonitor", 4096, NULL, 2, NULL);
   xTaskCreate(neo_blinky, "Task NEO Blink", 4096, NULL, 2, NULL);
@@ -30,7 +31,9 @@ void setup()
   xTaskCreate(lightAffectsLed, "Light Affects LED", 4096 , NULL, 2, NULL);
   xTaskCreate(soilMoistureAffectsPump, "Soil Moisture Affects Pump", 4096, NULL, 2, NULL);
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 4096, NULL, 2, NULL);
-
+  /********************************************************************************************/
+  
+  
   // xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
   // xTaskCreate( tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
   // xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,NULL  ,2 , NULL);
